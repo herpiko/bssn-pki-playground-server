9f7747e5142b2f602d03515a658095039b6d3a99
Herpiko Dwi Aguno <herpiko@gmail.com>
2018-09-19 00:34:21 +0700

    Initial commit.

---
+++ serve.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
+console.log('Running on localhost:3000');
